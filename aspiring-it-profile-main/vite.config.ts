a5fd0c41c93aa7e3e5861fb55542233b75f40df0
Vanaiyalini <kyalini2001@gmail.com>
2025-07-21 09:40:53 +0530

    Deployed is done!

--- aspiring-it-profile-main/vite.config.ts
+++ aspiring-it-profile-main/vite.config.ts
@@ -6,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: "/Portfolio/",
+    plugins: [react()],
     host: "::",
     port: 8080,
   },
